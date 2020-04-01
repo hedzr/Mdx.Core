9cda65376e27db07aabdf7dc033a277b3ebcc055
Hedzr Yeh <hedzrz@gmail.com>
2020-04-01 08:40:04 +0800

    mdx cmd

---
+++ src/mdx/Cmd/RootCmd.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using HzNS.Cmdr;
+using HzNS.Cmdr.Base;
+using HzNS.Cmdr.Internal.Base;
+using HzNS.MdxLib.MDict;
+
+namespace mdx.Cmd
+{
+    public class RootCmd : BaseRootCommand, IAction
+    {
+        private RootCmd(IAppInfo appInfo) : base(appInfo)
+        {
+        }
+
+        public static RootCmd New(IAppInfo appInfo, params Action<RootCmd>[] opts)
+        {
+            var r = new RootCmd(appInfo);
+            
+            foreach (var opt in opts)
+            {
+                opt(r);
+            }
+
+            return r;
+        }
+
+        public void Invoke(IBaseWorker w, IEnumerable<string> remainsArgs)
+        {
+            foreach (var filename in remainsArgs)
+            {
+                if (string.IsNullOrEmpty(filename)) continue;
+
+                w.log.logInfo($"loading {filename} ...");
+                using var l = new MDictLoader(filename);
+                try
+                {
+                    l.Process();
+                    Console.WriteLine($"header: {l.DictHeader}");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    // throw;
+                }
+                finally
+                {
+                    w.ParsedCount++;
+                    // w.log.Information($"#{w.ParsedCount} parsed.");
+
+                    // l.Dispose();
+                }
+            }
+
+            if (w.ParsedCount == 0)
+                w.log.logWarning(null, "Nothing to parsed.");
+        }
+    }
+}
---
+++ src/mdx/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HzNS.Cmdr;
+using HzNS.Cmdr.Base;
+using HzNS.Cmdr.Logger.Serilog;
+using mdx.Cmd;
+
+namespace mdx
+{
+    /// <summary>
+    ///
+    /// ll
+    /// </summary>
+    [SuppressMessage("ReSharper", "ArrangeTypeMemberModifiers")]
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    [SuppressMessage("ReSharper", "ArrangeTypeModifiers")]
+    [SuppressMessage("ReSharper", "CommentTypo")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cmdr: A CommandLine Arguments Parser
+            Cmdr.NewWorker(RootCmd.New(
+                        new AppInfo {AppName = "mdxTool", }, (root) =>
+                    {
+                        root.AddCommand(new Command {Short = "t", Long = "tags", Description = "tags operations"}
+                            .AddCommand(new TagsAddCmd())
+                            .AddCommand(new TagsRemoveCmd())
+                            // .AddCommand(new TagsAddCmd { }) // dup-test
+                            .AddCommand(new TagsListCmd())
+                            .AddCommand(new TagsModifyCmd())
+                        );
+                    }), // <- RootCmd
+                    // Options ->
+                    (w) =>
+                    {
+                        w.SetLogger(SerilogBuilder.Build((logger) =>
+                        {
+                            logger.EnableCmdrLogInfo = false;
+                            logger.EnableCmdrLogTrace = false;
+                        }));
+
+                        // w.EnableDuplicatedCharThrows = true;
+                    })
+                .Run(args);
+
+            // HzNS.MdxLib.Core.Open("*.mdx,mdd,sdx,wav,png,...") => mdxfile
+            // mdxfile.Preload()
+            // mdxfile.GetEntry("beta") => entryInfo.{item,index}
+            // mdxfile.Find("a")           // "a", "a*b", "*b"
+            // mdxfile.Close()
+            // mdxfile.Find()
+            // mdxfile.Find()
+            // mdxfile.Find()
+
+            // Log.CloseAndFlush();
+            // Console.ReadKey();
+        }
+    }
+}
